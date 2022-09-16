583b517b2ea407f6772c242bef98afdc293d1a9b
Christoph M. Wintersteiger <cwinter@microsoft.com>
2022-09-16 17:18:17 +0000

    Fix openssl emscripten instructions

--- wasm/openssl.sh
+++ wasm/openssl.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-source "/data/cwinter/emsdk/emsdk_env.sh"
+# Requires emscripten in the path
+source "~/emsdk/emsdk_env.sh"
 
 git clone git://git.openssl.org/openssl.git --depth 1 --branch OpenSSL_1_1_1-stable
 cd openssl
-emcmake ./Configure -no-asm -no-ssl3 -no-comp -no-engine -no-deprecated -shared -no-dso --openssldir=built linux-generic32 no-ssl2 no-hw 
-# -no-apps
-emmake make
+emconfigure ./Configure -no-asm -no-ssl3 -no-comp -no-engine -no-deprecated -shared -no-dso --openssldir=built linux-generic32 no-ssl2 no-hw
+sed -i "s/^CROSS_COMPILE=.*$/CROSS_COMPILE=/" Makefile
+emmake make build_generated libssl.a libcrypto.a
