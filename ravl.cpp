6bb72deb9a1bcc87b7f12fb7741ea3458b5bcea7
Christoph M. Wintersteiger <cwinter@microsoft.com>
2022-09-21 17:38:34 +0000

    Reorganize async data structures

--- ravl.cpp
+++ ravl.cpp
@@ -3,7 +3,6 @@
 
 #include "ravl.h"
 
-#include "ravl_attestation_request_tracker.h"
 #include "ravl_crypto.h"
 #include "ravl_sgx.h"
 #include "ravl_url_requests.h"
@@ -128,6 +127,252 @@ namespace ravl
       j["endorsements"] = to_base64(endorsements);
     }
     return j.dump();
+  }
+
+  class AttestationRequestTrackerImpl
+  {
+  public:
+    using RequestID = AttestationRequestTracker::RequestID;
+    using RequestState = AttestationRequestTracker::RequestState;
+    using Result = AttestationRequestTracker::Result;
+
+    struct Request
+    {
+      AttestationRequestTracker::RequestState state = RequestState::ERROR;
+      Options options;
+      std::shared_ptr<const Attestation> attestation;
+      AttestationRequestTracker::Result result = false;
+      std::optional<URLRequestSetId> request_set_id = std::nullopt;
+      std::shared_ptr<URLRequestTracker> url_request_tracker;
+    };
+
+    std::mutex requests_mtx;
+    std::map<AttestationRequestTracker::RequestID, Request> requests;
+    std::shared_ptr<URLRequestTracker> url_request_tracker;
+    AttestationRequestTracker::RequestID next_request_id = 0;
+
+    RequestID submit(
+      const Options& options,
+      std::shared_ptr<const Attestation> attestation,
+      std::shared_ptr<URLRequestTracker> request_tracker)
+    {
+      auto request_id = next_request_id++;
+
+      requests[request_id] = {
+        RequestState::SUBMITTED,
+        options,
+        attestation,
+        false,
+        0,
+        request_tracker};
+
+      return request_id;
+    }
+
+    RequestState state(RequestID id) const
+    {
+      auto rit = requests.find(id);
+      if (rit == requests.end())
+        return RequestState::ERROR;
+      else
+        return rit->second.state;
+    }
+
+    RequestID advance(RequestID id, Request& req)
+    {
+      switch (req.state)
+      {
+        case RequestState::ERROR:
+          throw std::runtime_error("verification request failed");
+        case RequestState::SUBMITTED:
+          if (!prepare_endorsements(req))
+            req.state = RequestState::HAVE_ENDORSEMENTS;
+          else
+            req.state = RequestState::WAITING_FOR_ENDORSEMENTS;
+          break;
+        case RequestState::WAITING_FOR_ENDORSEMENTS:
+          if (
+            !req.request_set_id ||
+            req.url_request_tracker->is_complete(*req.request_set_id))
+            req.state = RequestState::HAVE_ENDORSEMENTS;
+          break;
+        case RequestState::HAVE_ENDORSEMENTS:
+          verify(req);
+          req.state = RequestState::FINISHED;
+          break;
+        case RequestState::FINISHED:
+          break;
+        default:
+          throw std::runtime_error("unexpected request state");
+      }
+
+      return req.state;
+    }
+
+    bool finished(RequestID id) const
+    {
+      return state(id) == RequestState::FINISHED;
+    }
+
+    Result result(RequestID id) const
+    {
+      auto rit = requests.find(id);
+      if (rit == requests.end())
+        throw std::runtime_error("no such attestation verification request");
+      if (rit->second.state != RequestState::FINISHED)
+        throw std::runtime_error(
+          "attestation verification request not finished");
+      return rit->second.result;
+    }
+
+    void erase(RequestID id)
+    {
+      auto rit = requests.find(id);
+      if (rit != requests.end())
+        requests.erase(rit);
+    }
+
+    AttestationRequestTracker::RequestID advance(RequestID id)
+    {
+      auto rit = requests.find(id);
+      return advance(id, rit->second);
+    }
+
+    bool prepare_endorsements(Request& request)
+    {
+      if (!request.attestation)
+        throw std::runtime_error("no attestation to verify");
+
+      const auto& attestation = *request.attestation;
+      const auto& options = request.options;
+      auto request_tracker = request.url_request_tracker;
+
+      if (options.verbosity > 0)
+      {
+        json j;
+        to_json(j, attestation.source);
+        log(fmt::format("* Verifying attestation from {}", j.dump()));
+
+        log("- Options", 2);
+        if (options.fresh_endorsements)
+          log("- Fresh endorsements", 4);
+        if (options.fresh_root_ca_certificate)
+          log("- Fresh root CA certificate", 4);
+        if (options.root_ca_certificate)
+          log("- Custom root CA certificate", 4);
+        if (
+          options.certificate_verification.ignore_time ||
+          options.certificate_verification.verification_time)
+        {
+          log("- Certificate verification", 4);
+          if (options.certificate_verification.ignore_time)
+            log("- Ignore certificate times", 6);
+          if (options.certificate_verification.verification_time)
+            log("- Use custom certificate verification time", 6);
+        }
+      }
+
+      try
+      {
+        request.request_set_id =
+          request.attestation->prepare_endorsements(options, request_tracker);
+      }
+      catch (std::exception& ex)
+      {
+        if (options.verbosity > 0)
+          log(fmt::format("  - verification failed: {}", ex.what()));
+        throw std::runtime_error(
+          fmt::format("attestation verification failed: {}", ex.what()));
+      }
+
+      return request.request_set_id.has_value();
+    }
+
+    void verify(Request& request)
+    {
+      if (!request.attestation)
+        throw std::runtime_error("no attestation to verify");
+
+      auto& attestation = *request.attestation;
+      const auto& options = request.options;
+      auto request_tracker = request.url_request_tracker;
+
+      bool r = false;
+
+      std::vector<URLResponse> responses;
+
+      if (request.request_set_id)
+        responses =
+          request.url_request_tracker->collect(*request.request_set_id);
+
+      try
+      {
+        r = attestation.verify(options, responses);
+      }
+      catch (std::exception& ex)
+      {
+        if (options.verbosity > 0)
+          log(fmt::format("  - verification failed: {}", ex.what()));
+        throw std::runtime_error(
+          fmt::format("attestation verification failed: {}", ex.what()));
+      }
+
+      if (options.verbosity > 0)
+        log(fmt::format("  - verification successful"));
+
+      request.result = r;
+    }
+  };
+
+  AttestationRequestTracker::AttestationRequestTracker()
+  {
+    implementation = new AttestationRequestTrackerImpl();
+  }
+
+  AttestationRequestTracker::~AttestationRequestTracker()
+  {
+    delete static_cast<AttestationRequestTrackerImpl*>(implementation);
+  }
+
+  AttestationRequestTracker::RequestID AttestationRequestTracker::submit(
+    const Options& options,
+    std::shared_ptr<const Attestation> attestation,
+    std::shared_ptr<URLRequestTracker> url_request_tracker)
+  {
+    return static_cast<AttestationRequestTrackerImpl*>(implementation)
+      ->submit(options, attestation, url_request_tracker);
+  }
+
+  AttestationRequestTracker::RequestState AttestationRequestTracker::state(
+    RequestID id) const
+  {
+    return static_cast<AttestationRequestTrackerImpl*>(implementation)
+      ->state(id);
+  }
+
+  bool AttestationRequestTracker::finished(RequestID id) const
+  {
+    return static_cast<AttestationRequestTrackerImpl*>(implementation)
+      ->finished(id);
+  }
+
+  AttestationRequestTracker::Result AttestationRequestTracker::result(
+    RequestID id) const
+  {
+    return static_cast<AttestationRequestTrackerImpl*>(implementation)
+      ->result(id);
+  }
+
+  void AttestationRequestTracker::erase(RequestID id)
+  {
+    static_cast<AttestationRequestTrackerImpl*>(implementation)->erase(id);
+  }
+
+  AttestationRequestTracker::RequestID AttestationRequestTracker::advance(
+    RequestID id)
+  {
+    return static_cast<AttestationRequestTrackerImpl*>(implementation)
+      ->advance(id);
   }
 
   bool verify(
--- ravl.h
+++ ravl.h
@@ -6,11 +6,48 @@
 #include "ravl_attestation.h"
 #include "ravl_options.h"
 
+#include <map>
 #include <memory>
+#include <mutex>
 
 namespace ravl
 {
-  /// Synchronous verification of attestations (including endorsement download).
+  /// Tracker for asynchronous attestation verification
+  class AttestationRequestTracker
+  {
+  public:
+    AttestationRequestTracker();
+
+    virtual ~AttestationRequestTracker();
+
+    typedef size_t RequestID;
+    typedef bool Result;
+
+    enum RequestState
+    {
+      SUBMITTED = 0,
+      WAITING_FOR_ENDORSEMENTS,
+      HAVE_ENDORSEMENTS,
+      FINISHED,
+      ERROR
+    };
+
+    RequestID submit(
+      const Options& options,
+      std::shared_ptr<const Attestation> attestation,
+      std::shared_ptr<URLRequestTracker> request_tracker = nullptr);
+
+    RequestState state(RequestID id) const;
+    RequestID advance(RequestID id);
+    bool finished(RequestID id) const;
+    Result result(RequestID id) const;
+    void erase(RequestID id);
+
+  protected:
+    void* implementation;
+  };
+
+  /// Synchronous verification (including endorsement download).
   bool verify(
     std::shared_ptr<const Attestation> attestation,
     const Options& options,
