bbef4598e467188a9b5cdaf4887a9cf926fb4839
Christoph M. Wintersteiger <cwinter@microsoft.com>
2022-09-23 18:34:08 +0000

    More emscripten/fetch

--- ravl_url_requests_curl.cpp
+++ ravl_url_requests_curl.cpp
@@ -22,7 +22,6 @@
 namespace ravl
 {
   static bool initialized = false;
-  static std::mutex mtx;
 
   static size_t body_write_fun(
     char* ptr, size_t size, size_t nmemb, void* userdata)
@@ -163,227 +162,269 @@ namespace ravl
       return {hit->second.data(), hit->second.data() + hit->second.size()};
   }
 
-  AsynchronousURLRequestTracker::AsynchronousURLRequestTracker(bool verbose) :
-    URLRequestTracker(verbose)
-  {}
-
-  class AsynchronousURLRequestTracker::MonitorThread
+  class CurlTracker : public URLRequestTracker
   {
   public:
-    MonitorThread(
-      AsynchronousURLRequestTracker* tracker,
+    CurlTracker(bool verbose) : URLRequestTracker(verbose) {}
+
+    class MonitorThread
+    {
+    public:
+      MonitorThread(
+        CurlTracker* tracker,
+        URLRequestSetId id,
+        CURLM* multi,
+        std::function<void(URLResponses&&)> callback) :
+        keep_going(true),
+        tracker(tracker),
+        id(id),
+        multi(multi),
+        callback(callback)
+      {
+        t = std::thread(&MonitorThread::run, this);
+        t.detach();
+      }
+
+      virtual ~MonitorThread() {}
+
+      void stop()
+      {
+        keep_going = false;
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+      }
+
+      void run()
+      {
+        while (keep_going)
+          keep_going &= tracker->poll(id, multi, callback);
+      }
+
+    protected:
+      bool keep_going = true;
+      std::thread t;
+
+      CurlTracker* tracker;
+      URLRequestSetId id;
+      CURLM* multi;
+      std::function<void(URLResponses&&)> callback;
+    };
+
+    bool poll(
       URLRequestSetId id,
-      void* multi,
-      std::function<void(URLResponses&&)> callback) :
-      keep_going(true),
-      tracker(tracker),
-      id(id),
-      multi(multi),
-      callback(callback)
+      CURLM* multi,
+      std::function<void(URLResponses&&)>& callback)
     {
-      t = std::thread(&MonitorThread::run, this);
-      t.detach();
+      auto consume_msgs = [this, id, multi]() {
+        struct CURLMsg* m;
+        do
+        {
+          int msgq = 0;
+          m = curl_multi_info_read(multi, &msgq);
+          if (m && m->msg == CURLMSG_DONE)
+          {
+            size_t i = 0;
+            auto cc = curl_easy_getinfo(m->easy_handle, CURLINFO_PRIVATE, &i);
+            if (cc == CURLE_OK)
+              complete(id, i, m->easy_handle);
+          }
+        } while (m);
+      };
+
+      if (!is_complete(id))
+      {
+        std::lock_guard<std::mutex> guard(mtx);
+        int num_active_fds = 0;
+        CURLMcode mc = curl_multi_poll(multi, NULL, 0, 100, &num_active_fds);
+        if (mc != CURLM_OK)
+          throw std::runtime_error("curl_multi_poll failed");
+        consume_msgs();
+        return true;
+      }
+      else if (callback)
+      {
+        std::lock_guard<std::mutex> guard(mtx);
+        consume_msgs();
+        auto rsps_it = responses.find(id);
+        if (rsps_it == responses.end())
+          throw std::runtime_error("could not find url responses");
+        URLResponses rs;
+        rs.swap(rsps_it->second);
+        callback(std::move(rs));
+        responses.erase(rsps_it);
+        requests.erase(id);
+      }
+
+      curl_multi_cleanup(multi);
+
+      return false;
     }
 
-    virtual ~MonitorThread() {}
-
-    void stop()
+    URLRequestSetId submit(
+      URLRequests&& rs, std::function<void(URLResponses&&)>&& callback)
     {
-      keep_going = false;
-      std::this_thread::sleep_for(std::chrono::milliseconds(100));
+      std::lock_guard<std::mutex> guard(mtx);
+
+      if (!initialized)
+      {
+        curl_global_init(CURL_GLOBAL_ALL);
+        atexit(curl_global_cleanup);
+        initialized = true;
+      }
+
+      URLRequestSetId id = requests.size();
+      auto [it, ok] = requests.emplace(id, TrackedRequests{std::move(rs)});
+      if (!ok)
+        throw std::bad_alloc();
+
+      TrackedRequests& reqs = it->second;
+
+      CURLM* multi = curl_multi_init();
+
+      if (!multi)
+        throw std::bad_alloc();
+
+      reqs.multi = multi;
+
+      auto [rsps_it, rsps_ok] =
+        responses.emplace(id, URLResponses(reqs.requests.size()));
+      if (!rsps_ok)
+        throw std::bad_alloc();
+
+      std::vector<CURL*> easies;
+
+      for (size_t i = 0; i < reqs.requests.size(); i++)
+      {
+        auto& request = reqs.requests.at(i);
+        // printf("Submit   %zu: %s\n", i, request.url.c_str());
+        CURL* easy = curl_easy_init();
+        if (!easy)
+          throw std::bad_alloc();
+        URLResponse& response = rsps_it->second[i];
+        easy_setup(easy, request.url, request.body, response, verbose);
+        curl_easy_setopt(easy, CURLOPT_PRIVATE, i);
+        curl_multi_add_handle(multi, easy);
+        easies.push_back(easy);
+      }
+
+      int running_handles = 0;
+      CURLMcode curl_code = curl_multi_perform(multi, &running_handles);
+
+      if (curl_code != CURLM_OK)
+      {
+        for (const auto& easy : easies)
+        {
+          curl_multi_remove_handle(multi, easy);
+          curl_easy_cleanup(easy);
+        }
+        curl_multi_cleanup(multi);
+        throw std::runtime_error("curl_multi_perform unsuccessful");
+      }
+
+      monitor_threads[id] =
+        std::make_shared<MonitorThread>(this, id, multi, callback);
+
+      return id;
     }
 
-    void run()
+    void complete(size_t id, size_t i, CURL* easy)
     {
-      while (keep_going)
-        keep_going &= tracker->poll(id, multi, callback);
+      // Lock held by the monitor thread
+
+      auto rqit = requests.find(id);
+      if (rqit == requests.end())
+        return;
+
+      auto multi = rqit->second.multi;
+
+      auto& req = rqit->second.requests.at(i);
+
+      auto rsit = responses.find(id);
+      if (rsit == responses.end())
+        throw std::runtime_error("response set not found");
+
+      if (rsit->second.size() != rqit->second.requests.size())
+        rsit->second.resize(rqit->second.requests.size());
+
+      if (i >= rsit->second.size())
+        throw std::runtime_error("request index too large");
+
+      URLResponse& response = rsit->second.at(i);
+
+      curl_multi_remove_handle(multi, easy);
+      if (must_retry(easy, response, true))
+        curl_multi_add_handle(multi, easy);
+      else
+      {
+        curl_easy_cleanup(easy);
+        // printf(
+        //   "Complete %zu: %u size %zu (req. %zu)\n",
+        //   i,
+        //   response.status,
+        //   response.body.size(),
+        //   id);
+      }
+    }
+
+    bool is_complete(const URLRequestSetId& id) const
+    {
+      Requests::const_iterator rit = requests.end();
+      CURLMcode mc = CURLM_OK;
+      int still_running = 0;
+      CURLM* multi = NULL;
+
+      {
+        std::lock_guard<std::mutex> guard(mtx);
+        rit = requests.find(id);
+        if (rit == requests.end())
+          throw std::runtime_error("no such request set");
+
+        multi = rit->second.multi;
+        mc = curl_multi_perform(multi, &still_running);
+        return mc != CURLM_OK || still_running == 0;
+      }
     }
 
   protected:
-    bool keep_going = true;
-    std::thread t;
+    mutable std::mutex mtx;
 
-    AsynchronousURLRequestTracker* tracker;
-    URLRequestSetId id;
-    void* multi;
-    std::function<void(URLResponses&&)> callback;
-  };
-
-  bool AsynchronousURLRequestTracker::poll(
-    URLRequestSetId id,
-    void* multi,
-    std::function<void(URLResponses&&)>& callback)
-  {
-    auto consume_msgs = [this, id, multi]() {
-      struct CURLMsg* m;
-      do
-      {
-        int msgq = 0;
-        m = curl_multi_info_read(multi, &msgq);
-        if (m && m->msg == CURLMSG_DONE)
-        {
-          size_t i = 0;
-          auto cc = curl_easy_getinfo(m->easy_handle, CURLINFO_PRIVATE, &i);
-          if (cc == CURLE_OK)
-            complete(id, i, m->easy_handle);
-        }
-      } while (m);
+    struct TrackedRequests
+    {
+      URLRequests requests = {};
+      CURLM* multi = NULL;
+      std::function<void(URLResponses&&)> callback = nullptr;
     };
 
-    if (!is_complete(id))
-    {
-      std::lock_guard<std::mutex> guard(mtx);
-      int num_active_fds = 0;
-      CURLMcode mc = curl_multi_poll(multi, NULL, 0, 100, &num_active_fds);
-      if (mc != CURLM_OK)
-        throw std::runtime_error("curl_multi_poll failed");
-      consume_msgs();
-      return true;
-    }
-    else if (callback)
-    {
-      std::lock_guard<std::mutex> guard(mtx);
-      consume_msgs();
-      auto rsps_it = responses.find(id);
-      if (rsps_it == responses.end())
-        throw std::runtime_error("could not find url responses");
-      URLResponses rs;
-      rs.swap(rsps_it->second);
-      callback(std::move(rs));
-      responses.erase(rsps_it);
-      requests.erase(id);
-    }
+    typedef std::unordered_map<URLRequestSetId, std::shared_ptr<MonitorThread>>
+      MonitorThreads;
 
-    curl_multi_cleanup(multi);
+    MonitorThreads monitor_threads;
 
-    return false;
+    typedef std::unordered_map<URLRequestSetId, TrackedRequests> Requests;
+    Requests requests;
+
+    std::unordered_map<URLRequestSetId, URLResponses> responses;
+  };
+
+  AsynchronousURLRequestTracker::AsynchronousURLRequestTracker(bool verbose)
+  {
+    implementation = new CurlTracker(verbose);
+  }
+
+  AsynchronousURLRequestTracker::~AsynchronousURLRequestTracker()
+  {
+    delete static_cast<CurlTracker*>(implementation);
   }
 
   URLRequestSetId AsynchronousURLRequestTracker::submit(
     URLRequests&& rs, std::function<void(URLResponses&&)>&& callback)
   {
-    std::lock_guard<std::mutex> guard(mtx);
-
-    if (!initialized)
-    {
-      curl_global_init(CURL_GLOBAL_ALL);
-      atexit(curl_global_cleanup);
-      initialized = true;
-    }
-
-    URLRequestSetId id = requests.size();
-    auto [it, ok] = requests.emplace(id, TrackedRequests{std::move(rs)});
-    if (!ok)
-      throw std::bad_alloc();
-
-    TrackedRequests& reqs = it->second;
-
-    CURLM* multi = curl_multi_init();
-
-    if (!multi)
-      throw std::bad_alloc();
-
-    reqs.handle = multi;
-
-    auto [rsps_it, rsps_ok] =
-      responses.emplace(id, URLResponses(reqs.requests.size()));
-    if (!rsps_ok)
-      throw std::bad_alloc();
-
-    std::vector<void*> easies;
-
-    for (size_t i = 0; i < reqs.requests.size(); i++)
-    {
-      auto& request = reqs.requests.at(i);
-      // printf("Submit   %zu: %s\n", i, request.url.c_str());
-      CURL* easy = curl_easy_init();
-      if (!easy)
-        throw std::bad_alloc();
-      URLResponse& response = rsps_it->second[i];
-      easy_setup(easy, request.url, request.body, response, verbose);
-      curl_easy_setopt(easy, CURLOPT_PRIVATE, i);
-      curl_multi_add_handle(multi, easy);
-      easies.push_back(easy);
-    }
-
-    int running_handles = 0;
-    CURLMcode curl_code = curl_multi_perform(multi, &running_handles);
-
-    if (curl_code != CURLM_OK)
-    {
-      for (const auto& easy : easies)
-      {
-        curl_multi_remove_handle(multi, easy);
-        curl_easy_cleanup(easy);
-      }
-      curl_multi_cleanup(multi);
-      throw std::runtime_error("curl_multi_perform unsuccessful");
-    }
-
-    monitor_threads[id] =
-      std::make_shared<MonitorThread>(this, id, multi, callback);
-
-    return id;
-  }
-
-  void AsynchronousURLRequestTracker::complete(
-    size_t id, size_t i, void* handle)
-  {
-    // Lock held by the monitor thread
-
-    auto rqit = requests.find(id);
-    if (rqit == requests.end())
-      return;
-
-    auto multi = static_cast<CURLM*>(rqit->second.handle);
-    auto easy = static_cast<CURL*>(handle);
-
-    auto& req = rqit->second.requests.at(i);
-
-    auto rsit = responses.find(id);
-    if (rsit == responses.end())
-      throw std::runtime_error("response set not found");
-
-    if (rsit->second.size() != rqit->second.requests.size())
-      rsit->second.resize(rqit->second.requests.size());
-
-    if (i >= rsit->second.size())
-      throw std::runtime_error("request index too large");
-
-    URLResponse& response = rsit->second.at(i);
-
-    curl_multi_remove_handle(multi, easy);
-    if (must_retry(easy, response, true))
-      curl_multi_add_handle(multi, easy);
-    else
-    {
-      curl_easy_cleanup(easy);
-      // printf(
-      //   "Complete %zu: %u size %zu (req. %zu)\n",
-      //   i,
-      //   response.status,
-      //   response.body.size(),
-      //   id);
-    }
+    return static_cast<CurlTracker*>(implementation)
+      ->submit(std::move(rs), std::move(callback));
   }
 
   bool AsynchronousURLRequestTracker::is_complete(
     const URLRequestSetId& id) const
   {
-    Requests::const_iterator rit = requests.end();
-    CURLMcode mc = CURLM_OK;
-    int still_running = 0;
-    CURLM* multi = NULL;
-
-    {
-      std::lock_guard<std::mutex> guard(mtx);
-      rit = requests.find(id);
-      if (rit == requests.end())
-        throw std::runtime_error("no such request set");
-
-      multi = static_cast<CURL*>(rit->second.handle);
-      mc = curl_multi_perform(multi, &still_running);
-      return mc != CURLM_OK || still_running == 0;
-    }
+    return static_cast<CurlTracker*>(implementation)->is_complete(id);
   }
 }
