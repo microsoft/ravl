ab47d533cc01939a005b52a2351d43bd2ffc349f
Christoph M. Wintersteiger <cwinter@microsoft.com>
2022-10-03 11:35:55 +0000

    Some docstrings

--- include/ravl/attestation.h
+++ include/ravl/attestation.h
@@ -15,6 +15,7 @@
 
 namespace ravl
 {
+  /// Source enumeration
   enum class Source : uint8_t
   {
     SGX = 0,
@@ -23,19 +24,29 @@ namespace ravl
     UNKNOWN = UINT8_MAX
   };
 
+  /// Claims class
   class Claims
   {
   public:
+    /// Constructor
     Claims() : source(Source::UNKNOWN) {}
-    Claims(const Claims&) = default;
-    Claims(Claims&&) = default;
 
+    /// Constructor
     Claims(Source source) : source(source) {}
 
+    /// Copy constructor
+    Claims(const Claims&) = default;
+
+    /// Move constructor
+    Claims(Claims&&) = default;
+
+    /// Destructor
     virtual ~Claims() = default;
 
+    /// Source (platform) of the claims
     Source source;
 
+    /// Assignment operator
     Claims& operator=(const Claims&) = default;
 
     /// Function to up-cast generic claims
@@ -43,23 +54,35 @@ namespace ravl
     static std::shared_ptr<T> get(std::shared_ptr<ravl::Claims>& claims);
   };
 
+  /// Attestation class
   class Attestation
   {
   public:
+    /// Constructor
     Attestation() : source(Source::UNKNOWN) {}
 
+    /// Constructor
     Attestation(
       Source source,
       const std::vector<uint8_t>& evidence,
       const std::vector<uint8_t>& endorsements);
 
+    /// Copy constructor
     Attestation(const Attestation&) = default;
+
+    /// Move constructor
     Attestation(Attestation&&) = default;
 
+    /// Destructor
     virtual ~Attestation() {}
 
+    /// Source (platform) of the attestation
     Source source;
+
+    /// Evidence
     std::vector<uint8_t> evidence;
+
+    /// Endorsements
     std::vector<uint8_t> endorsements;
 
     /// Function to prepare network requests for endorsements
@@ -72,11 +95,14 @@ namespace ravl
       const Options& options,
       const std::optional<HTTPResponses>& http_response_set) const = 0;
 
+    /// String representation
     operator std::string() const;
 
+    /// Assignment operator
     Attestation& operator=(const Attestation& other) = default;
   };
 
+  // Parse a JSON-encoded attestation
   std::shared_ptr<Attestation> parse_attestation(
     const std::string& json_string);
 }
