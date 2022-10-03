d583be43ac4aa42229a490a3fa448e1454e0eae1
Christoph M. Wintersteiger <cwinter@microsoft.com>
2022-10-03 11:44:48 +0000

    Add CI workflows

--- test/unit_tests.cpp
+++ test/unit_tests.cpp
@@ -71,7 +71,7 @@ std::string ccf_quote = R"({
 
 std::shared_ptr<HTTPClient> http_client =
   std::make_shared<AsynchronousHTTPClient>(
-    /*request_timeout*/ 0, /*verbose=*/false);
+    /*request_timeout*/ 0, /*verbose=*/true);
 
 #ifndef USE_OE_VERIFIER
 // These attestations contain expired endorsements and the OE verifier doesn't
