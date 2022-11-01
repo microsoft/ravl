c6a791dd92fcf1e9e2ea4540aa9db102d4160cb8
Christoph M. Wintersteiger <cwinter@microsoft.com>
2022-11-01 12:54:59 +0000

    Remove extra namespace qualification

--- include/ravl/json_conversions.h
+++ include/ravl/json_conversions.h
@@ -14,16 +14,14 @@
 namespace nlohmann
 {
   template <size_t SZ>
-  struct nlohmann::adl_serializer<std::array<uint8_t, SZ>>
+  struct adl_serializer<std::array<uint8_t, SZ>>
   {
-    inline static void to_json(
-      nlohmann::json& j, const std::array<uint8_t, SZ>& x)
+    inline static void to_json(json& j, const std::array<uint8_t, SZ>& x)
     {
       j = ravl::to_hex(std::span(x));
     }
 
-    inline static void from_json(
-      const nlohmann::json& j, std::array<uint8_t, SZ>& x)
+    inline static void from_json(const json& j, std::array<uint8_t, SZ>& x)
     {
       auto tmp = ravl::vec_from_hex(j.get<std::string>());
       std::copy(tmp.begin(), tmp.end(), x.begin());
@@ -31,24 +29,23 @@ namespace nlohmann
   };
 
   template <>
-  struct nlohmann::adl_serializer<std::vector<uint8_t>>
+  struct adl_serializer<std::vector<uint8_t>>
   {
-    inline static void to_json(nlohmann::json& j, const std::vector<uint8_t>& x)
+    inline static void to_json(json& j, const std::vector<uint8_t>& x)
     {
       j = ravl::to_hex(x);
     }
 
-    inline static void from_json(
-      const nlohmann::json& j, std::vector<uint8_t>& x)
+    inline static void from_json(const json& j, std::vector<uint8_t>& x)
     {
       x = ravl::vec_from_hex(j.get<std::string>());
     }
   };
 
   template <typename T>
-  struct nlohmann::adl_serializer<std::optional<T>>
+  struct adl_serializer<std::optional<T>>
   {
-    inline static void to_json(nlohmann::json& j, const std::optional<T>& x)
+    inline static void to_json(json& j, const std::optional<T>& x)
     {
       if (x)
         j = *x;
@@ -56,7 +53,7 @@ namespace nlohmann
         j = nullptr;
     }
 
-    inline static void from_json(const nlohmann::json& j, std::optional<T>& x)
+    inline static void from_json(const json& j, std::optional<T>& x)
     {
       if (j == nullptr)
         x = std::nullopt;
